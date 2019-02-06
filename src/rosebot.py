0bcc3dd8563caa3e18e35e51404c69707306fe73
Joseph Law <lawjk@rose-hulman.edu>
2019-02-06 13:04:02 -0500

    Changed right motor to D

--- src/rosebot.py
+++ src/rosebot.py
@@ -208,7 +208,14 @@ class ArmAndClaw(object):
                (i.e., 14.2 motor revolutions),
           3. Resets the motor's position to 0.
         """
-
+        self.raise_arm()
+        self.motor.reset_position()
+        self.motor.turn_on(-100)
+        while True:
+            if abs(self.motor.get_position()) >= 360*14.2:
+                self.motor.turn_off()
+                break
+        self.motor.reset_position()
     def move_arm_to_position(self, desired_arm_position):
         """
         Move its Arm to the given position, where 0 means all the way DOWN.
