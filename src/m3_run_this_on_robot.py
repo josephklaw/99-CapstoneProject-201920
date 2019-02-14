929d2d3f45f74c7aa283a79fe2b70923e4d9664e
Lucas D'Alesio <dalesil@rose-hulman.edu>
2019-02-14 04:50:41 -0500

    Feature 9 in Progress

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -1,110 +1,110 @@
-"""
-  Capstone Project.  Code to run on the EV3 robot (NOT on a laptop).
-  Author:  Your professors (for the framework)
-    and Aaryan Khatri.
-  Winter term, 2018-2019.
-"""
-
-import rosebot
-import mqtt_remote_method_calls as com
-import time
-import shared_gui_delegate_on_robot
-
-def main():
-    """
-    This code, which must run on the EV3 ROBOT:
-      1. Makes the EV3 robot to various things.
-      2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
-    """
-    run_test_arm()
-    run_calibrate_arm()
-    real_thing()
-    #led_proximity_sensor()
-    #led()
-    #c
-
-
-
-def run_test_arm():
-    robot = rosebot.RoseBot()
-    robot.arm_and_claw.raise_arm()
-
-def run_calibrate_arm():
-    robot = rosebot.RoseBot()
-    robot.arm_and_claw.calibrate_arm()
-
-def real_thing():
-    robot = rosebot.RoseBot()
-    delegate_that_receives = shared_gui_delegate_on_robot.DelegateThatReceives(robot)
-    mqtt_receiver = com.MqttClient(delegate_that_receives)
-    mqtt_receiver.connect_to_pc()
-
-    while True:
-        time.sleep(0.01)
-        if delegate_that_receives.is_time_to_stop:
-            break
-
-def m3_led_proximity_sensor(initial, rate_of_increase):
-    robot = rosebot.RoseBot()
-    seconds = float(initial)
-    threshold = 20
-    robot.led_system.left_led.turn_off()
-    robot.led_system.right_led.turn_off()
-    robot.arm_and_claw.calibrate_arm()
-    robot.drive_system.go(50, 50)
-    while True:
-        distance = robot.sensor_system.ir_proximity_sensor.get_distance()
-        print(distance)
-        # Led Cycle
-        robot.led_system.left_led.turn_on()
-        time.sleep(secs / 4)
-        robot.led_system.left_led.turn_off()
-        robot.led_system.right_led.turn_on()
-        time.sleep(secs / 4)
-        robot.led_system.right_led.turn_off()
-        robot.led_system.left_led.turn_on()
-        robot.led_system.right_led.turn_on()
-        time.sleep(secs / 4)
-        robot.led_system.left_led.turn_off()
-        robot.led_system.right_led.turn_off()
-        time.sleep(secs / 4)
-        if distance < threshold:
-            robot.drive_system.stop()
-            robot.arm_and_claw.raise_arm()
-            break
-        increment = float(initial)/float(rate_of_increase)
-        sub = 100/increment
-        pos = 100 - distance
-        x = pos/sub
-        secs = initial - (float(rate_of_increase) * x)
-        if secs < 0:
-            secs = 0
-
-
-def led_test():
-    while True:
-        robot = rosebot.RoseBot()
-        robot.led_system.left_led.turn_on()
-        time.sleep(3)
-        robot.led_system.left_led.turn_off()
-        time.sleep(3)
-        robot.led_system.right_led.turn_on()
-        time.sleep(3)
-        robot.led_system.right_led.turn_off()
-        time.sleep(3)
-
-
-def m3_chose_pick_up(initial, rate_of_increase, direction, speed, area, string):
-    robot = rosebot.RoseBot()
-    if direction == 'Clockwise':
-        robot.drive_system.spin_clockwise_until_sees_object(speed, area)
-    elif direction == 'Counterclockwise':
-        robot.drive_system.spin_counterclockwise_until_sees_object(speed, area)
-    time.sleep(3)
-    if string == 'LED':
-        m3_led_proximity_sensor(initial, rate_of_increase)
-
-# -----------------------------------------------------------------------------
-# Calls  main  to start the ball rolling.
-# -----------------------------------------------------------------------------
-main()
+# """
+#   Capstone Project.  Code to run on the EV3 robot (NOT on a laptop).
+#   Author:  Your professors (for the framework)
+#     and Aaryan Khatri.
+#   Winter term, 2018-2019.
+# """
+#
+# import rosebot
+# import mqtt_remote_method_calls as com
+# import time
+# import shared_gui_delegate_on_robot
+#
+# def main():
+#     """
+#     This code, which must run on the EV3 ROBOT:
+#       1. Makes the EV3 robot to various things.
+#       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
+#     """
+#     run_test_arm()
+#     run_calibrate_arm()
+#     real_thing()
+#     #led_proximity_sensor()
+#     #led()
+#     #c
+#
+#
+#
+# def run_test_arm():
+#     robot = rosebot.RoseBot()
+#     robot.arm_and_claw.raise_arm()
+#
+# def run_calibrate_arm():
+#     robot = rosebot.RoseBot()
+#     robot.arm_and_claw.calibrate_arm()
+#
+# def real_thing():
+#     robot = rosebot.RoseBot()
+#     delegate_that_receives = shared_gui_delegate_on_robot.DelegateThatReceives(robot)
+#     mqtt_receiver = com.MqttClient(delegate_that_receives)
+#     mqtt_receiver.connect_to_pc()
+#
+#     while True:
+#         time.sleep(0.01)
+#         if delegate_that_receives.is_time_to_stop:
+#             break
+#
+# def m3_led_proximity_sensor(initial, rate_of_increase):
+#     robot = rosebot.RoseBot()
+#     seconds = float(initial)
+#     threshold = 20
+#     robot.led_system.left_led.turn_off()
+#     robot.led_system.right_led.turn_off()
+#     robot.arm_and_claw.calibrate_arm()
+#     robot.drive_system.go(50, 50)
+#     while True:
+#         distance = robot.sensor_system.ir_proximity_sensor.get_distance()
+#         print(distance)
+#         # Led Cycle
+#         robot.led_system.left_led.turn_on()
+#         time.sleep(secs / 4)
+#         robot.led_system.left_led.turn_off()
+#         robot.led_system.right_led.turn_on()
+#         time.sleep(secs / 4)
+#         robot.led_system.right_led.turn_off()
+#         robot.led_system.left_led.turn_on()
+#         robot.led_system.right_led.turn_on()
+#         time.sleep(secs / 4)
+#         robot.led_system.left_led.turn_off()
+#         robot.led_system.right_led.turn_off()
+#         time.sleep(secs / 4)
+#         if distance < threshold:
+#             robot.drive_system.stop()
+#             robot.arm_and_claw.raise_arm()
+#             break
+#         increment = float(initial)/float(rate_of_increase)
+#         sub = 100/increment
+#         pos = 100 - distance
+#         x = pos/sub
+#         secs = initial - (float(rate_of_increase) * x)
+#         if secs < 0:
+#             secs = 0
+#
+#
+# def led_test():
+#     while True:
+#         robot = rosebot.RoseBot()
+#         robot.led_system.left_led.turn_on()
+#         time.sleep(3)
+#         robot.led_system.left_led.turn_off()
+#         time.sleep(3)
+#         robot.led_system.right_led.turn_on()
+#         time.sleep(3)
+#         robot.led_system.right_led.turn_off()
+#         time.sleep(3)
+#
+#
+# def m3_chose_pick_up(initial, rate_of_increase, direction, speed, area, string):
+#     robot = rosebot.RoseBot()
+#     if direction == 'Clockwise':
+#         robot.drive_system.spin_clockwise_until_sees_object(speed, area)
+#     elif direction == 'Counterclockwise':
+#         robot.drive_system.spin_counterclockwise_until_sees_object(speed, area)
+#     time.sleep(3)
+#     if string == 'LED':
+#         m3_led_proximity_sensor(initial, rate_of_increase)
+#
+# # -----------------------------------------------------------------------------
+# # Calls  main  to start the ball rolling.
+# # -----------------------------------------------------------------------------
+# main()
--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -6,7 +6,7 @@
     and Joseph Law, Aaryan Khatri, and Lucas D'Alesio.
   Winter term, 2018-2019.
 """
-import m1_extra
+import m1_extra as m1
 import m2_extra as m2
 import m3_run_this_on_robot
 
@@ -14,7 +14,6 @@ class DelegateThatReceives(object):
     def __init__(self,robot):
         """:type  robot: rosebot.RoseBot"""
         self.robot = robot
-        self.m1 = m1_extra
         self.m3 = m3_run_this_on_robot
         self.is_time_to_stop = False
     def forward(self,left_wheel_speed,right_wheel_speed):
@@ -84,7 +83,7 @@ class DelegateThatReceives(object):
         self.robot.drive_system.go_until_distance_is_within(delta, inches, speed)
 
     def m1_proximity(self,initial_beep_rate,beep_rate_increase):
-        self.m1.increasing_rate_of_beep(initial_beep_rate,beep_rate_increase,self.robot)
+        m1.increasing_rate_of_beep(initial_beep_rate,beep_rate_increase,self.robot)
 
     def m2_proximity(self, initial_tone, tone_rate_increase, speed):
-        m2.increasing_tone(initial_tone, tone_rate_increase, speed, self.robot)
+        m2.increasing_tone(float(initial_tone), float(tone_rate_increase), int(speed), self.robot)
